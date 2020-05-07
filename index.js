e08c353ad2623cd288e41594f511ecbb7c501cda
DariusPirvulescu <organicdarius@gmail.com>
2020-05-07 14:57:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// var item = $('.carousel-item');
+
+// let just3 = $('#carousel1').children()[0]
+// console.log(item.length)
+// console.log(just3)
+
+
+// document.querySelector('#carousel1').childNodes[1]
